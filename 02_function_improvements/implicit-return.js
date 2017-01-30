db797236e440ed8003be204850c03d09d94c8a75
Daw-Chih Liou <dawochih.liou@gmail.com>
2017-01-30 11:27:09 -0800

    implicit return of arrow function

---
+++ 02_function_improvements/implicit-return.js
@@ -0,0 +1,7 @@
+/*
+ * Two things to note
+ *  1. Implicit return is the return value follow immediately by the arrow without {}
+ *  2. Arrow functions are always anonymou functions
+ */
+const anonymousFunction = () => 'La La Land'; // anonymousFunction returns a string
+console.log(anonymousFunction());
