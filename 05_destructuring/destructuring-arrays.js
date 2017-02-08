2664979ac8bafe5e0b35aa3f95ac33d27a727991
Daw-Chih Liou <dawochih.liou@gmail.com>
2017-02-08 20:16:30 +0800

    destructuring arrays

---
+++ 05_destructuring/destructuring-arrays.js
@@ -0,0 +1,8 @@
+/*
+ * destructuring array with rest operator to assign the rest of the array to the last 
+ * variable.
+ */
+const movies = ['La La Land', 'Girl on The Train', 'The Accountant', 'Inferno', 'Antman'];
+const [ musicalMovie, movieFromNovel, ...greatMovies ] = movies;
+
+
