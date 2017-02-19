d741240824101da4ad7c433d6c1e6c8f8939a04c
Daw-Chih Liou <dawochih.liou@gmail.com>
2017-02-19 11:20:55 +0800

    generator and ajax

---
+++ 16_generatrs/generator-ajax.js
@@ -0,0 +1,22 @@
+/*
+ * API reference: https://shkspr.mobi/blog/2014/04/wanted-simple-apis-without-authentication/
+ */
+function* fetchApis () {
+  const bbcEndpoint = 'http://www.bbc.co.uk/radio1/playlist.json';
+  const mapsEndpoint = 'https://maps.googleapis.com/maps/api/geocode/json?address=Oxford%20U    niversity,%20uk&sensor=false';
+  const booksEndpoint = 'https://www.googleapis.com/books/v1/volumes?q=isbn:0747532699v';
+  
+  const bbc = yield ajax(bbcEndpoint);
+  const maps = yield ajax(mapsEndpoint);
+  const books = yield ajax(booksEndpoint);
+}
+
+// fetch and parse as json and move on to the next api call
+function ajax (url) {
+  fetch(url)
+    .then(res => res.json())
+    .then(data => dataGenerator.next(data))
+}
+
+const dataGenerator = fetchApis();
+dataGenerator.next(); // kick it off
